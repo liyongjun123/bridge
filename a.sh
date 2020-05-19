d44184229c7f5e3d70975ad55ff0a3135b92a019
truebox <1160606604@qq.com>
2020-05-19 02:04:49 +0000

    2020-05-19 10:04

---
+++ a.sh
@@ -0,0 +1,12 @@
+sudo ip link set enp2s0 up
+sudo ip link set enp3s0 up
+
+sudo rmmod bridge
+sudo insmod bridge.ko
+
+sudo brctl addbr br0
+sudo brctl addif br0 enp2s0
+sudo brctl addif br0 enp3s0
+
+sudo ip link set enp2s0 up
+
---
+++ m.sh
@@ -0,0 +1,1 @@
+sudo make -C /lib/modules/4.15.0-101-generic/build/ M=/usr/src/linux-headers-4.15.0-101-generic/net/bridge modules
